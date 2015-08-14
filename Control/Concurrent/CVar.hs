0e558b271f5f1622119393bfd2125b69ed9873fa
Michael Walker <mike@barrucadu.co.uk>
2015-08-14 15:05:36 +0100

    Handle exceptions in CVar functions as in MVar functions

--- Control/Concurrent/CVar.hs
+++ Control/Concurrent/CVar.hs
@@ -1,8 +1,5 @@
 -- | Combinators using @CVar@s. These provide many of the helpful
--- functions found in Control.Concurrent.MVar, but for @CVar@s. Note
--- that these do not in general mask exceptions, and are not atomic,
--- being implemented in terms of the primitives in the 'MonadConc'
--- typeclass.
+-- functions found in Control.Concurrent.MVar, but for @CVar@s.
 module Control.Concurrent.CVar
  ( -- *@CVar@s
   CVar
@@ -16,8 +13,11 @@ module Control.Concurrent.CVar
  , tryPutCVar
  , isEmptyCVar
  , withCVar
+ , withCVarMasked
  , modifyCVar_
  , modifyCVar
+ , modifyCVarMasked_
+ , modifyCVarMasked
 
  -- * Binary semaphores
  -- | A common use of @CVar@s is in making binary semaphores to
@@ -28,6 +28,7 @@ module Control.Concurrent.CVar
  ) where
 
 import Control.Monad (liftM)
+import Control.Monad.Catch (mask_, onException)
 import Control.Monad.Conc.Class
 
 -- | Create a new @CVar@ containing a value.
@@ -37,9 +38,11 @@ newCVar a = do
   putCVar cvar a
   return cvar
 
--- | Swap the contents of a @CVar@, and return the value taken.
+-- | Swap the contents of a @CVar@, and return the value taken. This
+-- function is atomic only if there are no other producers fro this
+-- @CVar@.
 swapCVar :: MonadConc m => CVar m a -> a -> m a
-swapCVar cvar a = do
+swapCVar cvar a = mask_ $ do
   old <- takeCVar cvar
   putCVar cvar a
   return old
@@ -53,27 +56,63 @@ isEmptyCVar cvar = do
     Nothing   -> return False
 
 -- | Operate on the contents of a @CVar@, replacing the contents after
--- finishing.
+-- finishing. This operation is exception-safe: it will replace the
+-- original contents of the @CVar@ if an exception is raised. However,
+-- it is only atomic if there are no other producers for this @CVar@.
+{-# INLINE withCVar #-}
 withCVar :: MonadConc m => CVar m a -> (a -> m b) -> m b
-withCVar cvar f = do
+withCVar cvar f = mask $ \restore -> do
   val <- takeCVar cvar
-  out <- f val
+  out <- restore (f val) `onException` putCVar cvar val
   putCVar cvar val
 
   return out
 
--- | Apply a function to the value inside a @CVar@, and also return a
--- value.
-modifyCVar :: MonadConc m => CVar m a -> (a -> m (a, b)) -> m b
-modifyCVar cvar f = do
+-- | Like 'withCVar', but the @IO@ action in the second argument is
+-- executed with asynchronous exceptions masked.
+{-# INLINE withCVarMasked #-}
+withCVarMasked :: MonadConc m => CVar m a -> (a -> m b) -> m b
+withCVarMasked cvar f = mask_ $ do
   val <- takeCVar cvar
-  (val', out) <- f val
+  out <- f val `onException` putCVar cvar val
+  putCVar cvar val
+
+  return out
+
+-- | An exception-safe wrapper for modifying the contents of a @CVar@.
+-- Like 'withCVar', 'modifyCVar' will replace the original contents of
+-- the @CVar@ if an exception is raised during the operation. This
+-- function is only atomic if there are no other producers for this
+-- @CVar@.
+{-# INLINE modifyCVar_ #-}
+modifyCVar_ :: MonadConc m => CVar m a -> (a -> m a) -> m ()
+modifyCVar_ cvar f = modifyCVar cvar $ liftM (\a -> (a,())) . f
+
+-- | A slight variation on 'modifyCVar_' that allows a value to be
+-- returned (@b@) in addition to the modified value of the @CVar@.
+{-# INLINE modifyCVar #-}
+modifyCVar :: MonadConc m => CVar m a -> (a -> m (a, b)) -> m b
+modifyCVar cvar f = mask $ \restore -> do
+  val <- takeCVar cvar
+  (val', out) <- restore (f val) `onException` putCVar cvar val
   putCVar cvar val'
   return out
 
--- | Modify the contents of a @CVar@.
-modifyCVar_ :: MonadConc m => CVar m a -> (a -> m a) -> m ()
-modifyCVar_ cvar f = modifyCVar cvar $ \a -> (\b -> (b, ())) `liftM` f a
+-- | Like 'modifyCVar_', but the @IO@ action in the second argument is
+-- executed with asynchronous exceptions masked.
+{-# INLINE modifyCVarMasked_ #-}
+modifyCVarMasked_ :: MonadConc m => CVar m a -> (a -> m a) -> m ()
+modifyCVarMasked_ cvar f = modifyCVarMasked cvar $ liftM (\a -> (a,())) . f
+
+-- | Like 'modifyCVar', but the @IO@ action in the second argument is
+-- executed with asynchronous exceptions masked.
+{-# INLINE modifyCVarMasked #-}
+modifyCVarMasked :: MonadConc m => CVar m a -> (a -> m (a, b)) -> m b
+modifyCVarMasked cvar f = mask_ $ do
+  val <- takeCVar cvar
+  (val', out) <- f val `onException` putCVar cvar val
+  putCVar cvar val'
+  return out
 
 -- | Put a @()@ into a @CVar@, claiming the lock. This is atomic.
 lock :: MonadConc m => CVar m () -> m ()
--- Control/Concurrent/CVar/Strict.hs
+++ Control/Concurrent/CVar/Strict.hs
@@ -14,8 +14,11 @@ module Control.Concurrent.CVar.Strict
  , tryPutCVar
  , isEmptyCVar
  , withCVar
+ , withCVarMasked
  , modifyCVar_
  , modifyCVar
+ , modifyCVarMasked_
+ , modifyCVarMasked
 
  -- * Binary semaphores
  -- | A common use of @CVar@s is in making binary semaphores to
@@ -25,9 +28,10 @@ module Control.Concurrent.CVar.Strict
  , unlock
  ) where
 
-import Control.Concurrent.CVar (isEmptyCVar, withCVar, lock, unlock)
+import Control.Concurrent.CVar (isEmptyCVar, withCVar, withCVarMasked, lock, unlock)
 import Control.DeepSeq (NFData, force)
 import Control.Monad (liftM)
+import Control.Monad.Catch (mask_, onException)
 import Control.Monad.Conc.Class hiding (newEmptyCVar, putCVar, tryPutCVar)
 
 import qualified Control.Concurrent.CVar  as V
@@ -45,19 +49,6 @@ newCVar = V.newCVar . force
 swapCVar :: (MonadConc m, NFData a) => CVar m a -> a -> m a
 swapCVar cvar = V.swapCVar cvar . force
 
--- | Apply a function to the value inside a @CVar@, and also return a
--- value.
-modifyCVar :: (MonadConc m, NFData a) => CVar m a -> (a -> m (a, b)) -> m b
-modifyCVar cvar f = do
-  val <- takeCVar cvar
-  (val', out) <- f val
-  putCVar cvar val'
-  return out
-
--- | Modify the contents of a @CVar@.
-modifyCVar_ :: (MonadConc m, NFData a) => CVar m a -> (a -> m a) -> m ()
-modifyCVar_ cvar f = modifyCVar cvar $ \a -> (\b -> (b, ())) `liftM` f a
-
 -- | Put a value into a @CVar@. If there is already a value there,
 -- this will block until that value has been taken, at which point the
 -- value will be stored.
@@ -69,3 +60,38 @@ putCVar cvar = C.putCVar cvar . force
 -- 'False'.
 tryPutCVar :: (MonadConc m, NFData a) => CVar m a -> a -> m Bool
 tryPutCVar cvar = C.tryPutCVar cvar . force
+
+-- | An exception-safe wrapper for modifying the contents of a @CVar@.
+-- Like 'withCVar', 'modifyCVar' will replace the original contents of
+-- the @CVar@ if an exception is raised during the operation. This
+-- function is only atomic if there are no other producers for this
+-- @CVar@.
+{-# INLINE modifyCVar_ #-}
+modifyCVar_ :: (MonadConc m, NFData a) => CVar m a -> (a -> m a) -> m ()
+modifyCVar_ cvar f = modifyCVar cvar $ liftM (\a -> (a,())) . f
+
+-- | A slight variation on 'modifyCVar_' that allows a value to be
+-- returned (@b@) in addition to the modified value of the @CVar@.
+{-# INLINE modifyCVar #-}
+modifyCVar :: (MonadConc m, NFData a) => CVar m a -> (a -> m (a, b)) -> m b
+modifyCVar cvar f = mask $ \restore -> do
+  val <- takeCVar cvar
+  (val', out) <- restore (f val) `onException` putCVar cvar val
+  putCVar cvar val'
+  return out
+
+-- | Like 'modifyCVar_', but the @IO@ action in the second argument is
+-- executed with asynchronous exceptions masked.
+{-# INLINE modifyCVarMasked_ #-}
+modifyCVarMasked_ :: (MonadConc m, NFData a) => CVar m a -> (a -> m a) -> m ()
+modifyCVarMasked_ cvar f = modifyCVarMasked cvar $ liftM (\a -> (a,())) . f
+
+-- | Like 'modifyCVar', but the @IO@ action in the second argument is
+-- executed with asynchronous exceptions masked.
+{-# INLINE modifyCVarMasked #-}
+modifyCVarMasked :: (MonadConc m, NFData a) => CVar m a -> (a -> m (a, b)) -> m b
+modifyCVarMasked cvar f = mask_ $ do
+  val <- takeCVar cvar
+  (val', out) <- f val `onException` putCVar cvar val
+  putCVar cvar val'
+  return out
