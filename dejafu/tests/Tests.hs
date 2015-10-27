3ad9fac8e975b4403b7e829bbb2e0a22a27e247b
Michael Walker <mike@barrucadu.co.uk>
2015-10-27 17:59:00 +0000

    Fix dejafu testsuite

--- dejafu/tests/Tests.hs
+++ dejafu/tests/Tests.hs
@@ -22,7 +22,7 @@ tests = TestList
     , testDejafu  C.threadKill      "Kill Thread"          deadlocksSometimes
     , testDejafu  C.threadKillMask  "Kill Thread w/ Mask"  deadlocksNever
     , testDejafu  C.threadKillUmask "Kill Thread w/ Umask" deadlocksSometimes
-    , testDejafu  C.stmAtomic       "STM Atomicity"      $ gives [0,2]
+    , testDejafu  C.stmAtomic       "STM Atomicity"      $ gives' [0,2]
     , testDejafu  C.stmRetry        "STM Retry"            alwaysSame
     , testDejafu  C.stmOrElse       "STM orElse"           alwaysSame
     , testDejafu  C.stmExc          "STM Exceptions"       alwaysSame
