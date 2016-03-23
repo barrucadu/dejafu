ef133a5d06492c1d280e6e837a738ff12bbe140e
Michael Walker <mike@barrucadu.co.uk>
2016-03-23 04:20:26 +0000

    Add classy STM TQueue

--- dejafu/Control/Concurrent/STM/Classy.hs
+++ dejafu/Control/Concurrent/STM/Classy.hs
@@ -4,9 +4,11 @@ module Control.Concurrent.STM.Classy
   , module Control.Concurrent.STM.Classy.TVar
   , module Control.Concurrent.STM.Classy.TMVar
   , module Control.Concurrent.STM.Classy.TChan
+  , module Control.Concurrent.STM.Classy.TQueue
   ) where
 
 import Control.Monad.STM.Class
 import Control.Concurrent.STM.Classy.TVar
 import Control.Concurrent.STM.Classy.TMVar
 import Control.Concurrent.STM.Classy.TChan
+import Control.Concurrent.STM.Classy.TQueue
