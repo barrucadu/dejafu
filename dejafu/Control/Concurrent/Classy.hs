f7d0059b7e16e8fe5a435887d1c79775a298344b
Michael Walker <mike@barrucadu.co.uk>
2016-03-26 06:42:57 +0000

    Add CRef module

--- dejafu/Control/Concurrent/Classy.hs
+++ dejafu/Control/Concurrent/Classy.hs
@@ -3,6 +3,7 @@ module Control.Concurrent.Classy (module All) where
 
 import Control.Monad.Conc.Class        as All
 import Control.Concurrent.Classy.Chan  as All
+import Control.Concurrent.Classy.CRef  as All
 import Control.Concurrent.Classy.MVar  as All
 import Control.Concurrent.Classy.STM   as All
 import Control.Concurrent.Classy.QSem  as All
