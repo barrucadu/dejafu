ad33fb729bc9aa92e5de732d62f8221d4b17bb67
Michael Walker <mike@barrucadu.co.uk>
2016-03-26 06:50:47 +0000

    Drop import/export shortcut for better haddocks

--- dejafu/Control/Concurrent/Classy.hs
+++ dejafu/Control/Concurrent/Classy.hs
@@ -1,10 +1,20 @@
 -- | Classy concurrency.
-module Control.Concurrent.Classy (module All) where
+module Control.Concurrent.Classy
+  ( module Control.Monad.Conc.Class
+  , module Control.Concurrent.Classy.Chan
+  , module Control.Concurrent.Classy.CRef
+  , module Control.Concurrent.Classy.MVar
+  , module Control.Concurrent.Classy.STM
+  , module Control.Concurrent.Classy.QSem
+  , module Control.Concurrent.Classy.QSemN
+  ) where
 
-import Control.Monad.Conc.Class        as All
-import Control.Concurrent.Classy.Chan  as All
-import Control.Concurrent.Classy.CRef  as All
-import Control.Concurrent.Classy.MVar  as All
-import Control.Concurrent.Classy.STM   as All
-import Control.Concurrent.Classy.QSem  as All
-import Control.Concurrent.Classy.QSemN as All
+import Control.Monad.Conc.Class
+import Control.Concurrent.Classy.Chan
+import Control.Concurrent.Classy.CRef
+import Control.Concurrent.Classy.MVar
+import Control.Concurrent.Classy.STM
+import Control.Concurrent.Classy.QSem
+import Control.Concurrent.Classy.QSemN
+
+{-# ANN module ("HLint: ignore Use import/export shortcut" :: String) #-}
--- dejafu/Control/Concurrent/Classy/STM.hs
+++ dejafu/Control/Concurrent/Classy/STM.hs
@@ -1,10 +1,20 @@
 -- | Classy software transactional memory.
-module Control.Concurrent.Classy.STM (module All) where
+module Control.Concurrent.Classy.STM
+  ( module Control.Monad.STM.Class
+  , module Control.Concurrent.Classy.STM.TVar
+  , module Control.Concurrent.Classy.STM.TMVar
+  , module Control.Concurrent.Classy.STM.TChan
+  , module Control.Concurrent.Classy.STM.TQueue
+  , module Control.Concurrent.Classy.STM.TBQueue
+  , module Control.Concurrent.Classy.STM.TArray
+  ) where
 
-import Control.Monad.STM.Class               as All
-import Control.Concurrent.Classy.STM.TVar    as All
-import Control.Concurrent.Classy.STM.TMVar   as All
-import Control.Concurrent.Classy.STM.TChan   as All
-import Control.Concurrent.Classy.STM.TQueue  as All
-import Control.Concurrent.Classy.STM.TBQueue as All
-import Control.Concurrent.Classy.STM.TArray  as All
+import Control.Monad.STM.Class
+import Control.Concurrent.Classy.STM.TVar
+import Control.Concurrent.Classy.STM.TMVar
+import Control.Concurrent.Classy.STM.TChan
+import Control.Concurrent.Classy.STM.TQueue
+import Control.Concurrent.Classy.STM.TBQueue
+import Control.Concurrent.Classy.STM.TArray
+
+{-# ANN module ("HLint: ignore Use import/export shortcut" :: String) #-}
