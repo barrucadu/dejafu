7733c762c158d089c99af73da937dec967358565
Michael Walker <mike@barrucadu.co.uk>
2016-03-30 04:48:41 +0100

    Merge the SCT modules

--- dejafu/Test/DejaFu/SCT.hs
+++ dejafu/Test/DejaFu/SCT.hs
@@ -91,17 +91,19 @@ module Test.DejaFu.SCT
   ) where
 
 import Data.Functor.Identity (Identity(..), runIdentity)
-import Data.Maybe (isJust)
-import Test.DejaFu.Deterministic
-import Test.DejaFu.Deterministic.Internal (initialThread, preEmpCount, willRelease)
-import Test.DejaFu.SCT.Internal
-import Test.DPOR ( BacktrackStep(..), backtrackAt
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Maybe (isJust, fromJust)
+import Test.DPOR ( DPOR(..), dpor
+                 , BacktrackStep(..), backtrackAt
                  , BoundFunc, (&+&), trueBound
                  , PreemptionBound(..), defaultPreemptionBound, preempBacktrack
                  , FairBound(..), defaultFairBound, fairBound, fairBacktrack
                  , LengthBound(..), defaultLengthBound, lenBound, lenBacktrack
-                 , dpor
                  )
+
+import Test.DejaFu.Deterministic (ConcIO, ConcST, runConcIO, runConcST)
+import Test.DejaFu.Deterministic.Internal
 
 -------------------------------------------------------------------------------
 -- Combined Bounds
@@ -198,7 +200,7 @@ pBacktrack :: [BacktrackStep ThreadId ThreadAction Lookahead s]
   -> ThreadId
   -- ^ The thread to backtrack to.
   -> [BacktrackStep ThreadId ThreadAction Lookahead s]
-pBacktrack = preempBacktrack isCommit
+pBacktrack = preempBacktrack isCommitRef
 
 -- | Pre-emption bound function. This is different to @preempBound@ in
 -- that it does not count pre-emptive context switches to a commit
@@ -322,12 +324,127 @@ sctBoundedM memtype bf backtrack run =
        (run memtype)
 
 -------------------------------------------------------------------------------
+-- Post-processing
+
+-- | Remove commits from the todo sets where every other action will
+-- result in a write barrier (and so a commit) occurring.
+--
+-- To get the benefit from this, do not execute commit actions from
+-- the todo set until there are no other choises.
+pruneCommits :: DPOR ThreadId ThreadAction -> DPOR ThreadId ThreadAction
+pruneCommits bpor
+  | not onlycommits || not alldonesync = go bpor
+  | otherwise = go bpor { dporTodo = M.empty }
+
+  where
+    go b = b { dporDone = pruneCommits <$> dporDone bpor }
+
+    onlycommits = all (<initialThread) . M.keys $ dporTodo bpor
+    alldonesync = all barrier . M.elems $ dporDone bpor
+
+    barrier = isBarrier . simplify . fromJust . dporAction
+
+-------------------------------------------------------------------------------
+-- Dependency function
+
+-- | Check if an action is dependent on another.
+dependent :: MemType -> CRState -> (ThreadId, ThreadAction) -> (ThreadId, ThreadAction) -> Bool
+dependent _ _ (_, Lift) (_, Lift) = True
+dependent _ _ (_, ThrowTo t) (t2, Stop) | t == t2 = False
+dependent _ _ (t2, Stop) (_, ThrowTo t) | t == t2 = False
+dependent _ _ (_, ThrowTo t) (t2, _) = t == t2
+dependent _ _ (t2, _) (_, ThrowTo t) = t == t2
+dependent _ _ (_, STM _ _) (_, STM _ _) = True
+dependent _ _ (_, GetNumCapabilities a) (_, SetNumCapabilities b) = a /= b
+dependent _ _ (_, SetNumCapabilities a) (_, GetNumCapabilities b) = a /= b
+dependent _ _ (_, SetNumCapabilities a) (_, SetNumCapabilities b) = a /= b
+dependent memtype buf (_, d1) (_, d2) = dependentActions memtype buf (simplify d1) (simplify d2)
+
+-- | Variant of 'dependent' to handle 'ThreadAction''s
+dependent' :: MemType -> CRState -> (ThreadId, ThreadAction) -> (ThreadId, Lookahead) -> Bool
+dependent' _ _ (_, Lift) (_, WillLift) = True
+dependent' _ _ (_, ThrowTo t) (t2, WillStop) | t == t2 = False
+dependent' _ _ (t2, Stop) (_, WillThrowTo t) | t == t2 = False
+dependent' _ _ (_, ThrowTo t) (t2, _)     = t == t2
+dependent' _ _ (t2, _) (_, WillThrowTo t) = t == t2
+dependent' _ _ (_, STM _ _) (_, WillSTM) = True
+dependent' _ _ (_, GetNumCapabilities a) (_, WillSetNumCapabilities b) = a /= b
+dependent' _ _ (_, SetNumCapabilities _) (_, WillGetNumCapabilities)   = True
+dependent' _ _ (_, SetNumCapabilities a) (_, WillSetNumCapabilities b) = a /= b
+-- This is safe because, if the thread blocks anyway, a context switch
+-- will occur anyway so there's no point pre-empting the action.
+--
+-- UNLESS the pre-emption would possibly allow for a different relaxed
+-- memory stage.
+dependent' _ _ (_, a1) (_, a2) | isBlock a1 && isBarrier (simplify' a2) = False
+dependent' memtype buf (_, d1) (_, d2) = dependentActions memtype buf (simplify d1) (simplify' d2)
+
+-- | Check if two 'ActionType's are dependent. Note that this is not
+-- sufficient to know if two 'ThreadAction's are dependent, without
+-- being so great an over-approximation as to be useless!
+dependentActions :: MemType -> CRState -> ActionType -> ActionType -> Bool
+dependentActions memtype buf a1 a2 = case (a1, a2) of
+  -- Unsynchronised reads and writes are always dependent, even under
+  -- a relaxed memory model, as an unsynchronised write gives rise to
+  -- a commit, which synchronises.
+  (UnsynchronisedRead  r1, UnsynchronisedWrite r2) -> r1 == r2
+  (UnsynchronisedWrite r1, UnsynchronisedRead  r2) -> r1 == r2
+  (UnsynchronisedWrite r1, UnsynchronisedWrite r2) -> r1 == r2
+
+  -- Unsynchronised writes and synchronisation where the buffer is not
+  -- empty.
+  --
+  -- See [RMMVerification], lemma 5.25.
+  (UnsynchronisedWrite r1, _) | same crefOf && isCommit a2 r1 && isBuffered buf r1 -> False
+  (_, UnsynchronisedWrite r2) | same crefOf && isCommit a1 r2 && isBuffered buf r2 -> False
+
+  -- Unsynchronised reads where a memory barrier would flush a
+  -- buffered write
+  (UnsynchronisedRead r1, _) | isBarrier a2 -> isBuffered buf r1 && memtype /= SequentialConsistency
+  (_, UnsynchronisedRead r2) | isBarrier a1 -> isBuffered buf r2 && memtype /= SequentialConsistency
+
+  (_, _)
+    -- Two actions on the same CRef where at least one is synchronised
+    | same crefOf && (synchronises a1 (fromJust $ crefOf a1) || synchronises a2 (fromJust $ crefOf a2)) -> True
+    -- Two actions on the same MVar
+    | same cvarOf -> True
+
+  _ -> False
+
+  where
+    same f = isJust (f a1) && f a1 == f a2
+
+-------------------------------------------------------------------------------
+-- Dependency function state
+
+type CRState = Map CRefId Bool
+
+-- | Initial global 'CRef buffer state.
+initialCRState :: CRState
+initialCRState = M.empty
+
+-- | Update the 'CRef' buffer state with the action that has just
+-- happened.
+updateCRState :: CRState -> ThreadAction -> CRState
+updateCRState crstate (CommitRef _ r) = M.delete r crstate
+updateCRState crstate (WriteRef r) = M.insert r True crstate
+updateCRState crstate ta
+  | isBarrier $ simplify ta = initialCRState
+  | otherwise = crstate
+
+-- | Check if a 'CRef' has a buffered write pending.
+--
+-- If the state is @Unknown@, this assumes @True@.
+isBuffered :: CRState -> CRefId -> Bool
+isBuffered crstate r = M.findWithDefault False r crstate
+
+-------------------------------------------------------------------------------
 -- Utilities
 
 -- | Determine if an action is a commit or not.
-isCommit :: ThreadAction -> Bool
-isCommit (CommitRef _ _) = True
-isCommit _ = False
+isCommitRef :: ThreadAction -> Bool
+isCommitRef (CommitRef _ _) = True
+isCommitRef _ = False
 
 -- | Check if a thread yielded.
 didYield :: ThreadAction -> Bool
