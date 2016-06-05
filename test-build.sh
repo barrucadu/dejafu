83a0d4e22902b326197c31538a9431724f5ffdca
Michael Walker <mike@barrucadu.co.uk>
2016-06-05 22:03:30 +0100

    Test building dejafu against dpor-0.1

--- test-build.sh
+++ test-build.sh
@@ -15,6 +15,23 @@ function testcmd()
   fi
 }
 
+function testdejafu()
+{
+  local name=$1
+  shift
+  local stackopts=$*
+
+  echo "== $name"
+  if ! stack $stackopts build dejafu-tests; then
+    echo "== FAILED (build)"
+    exit 1
+  fi
+  if ! stack $stackopts exec dejafu-tests; then
+    echo "== FAILED (test)"
+    exit 1
+  fi
+}
+
 # Set the resolver. Uses the environment variable "RESOLVER" if set,
 # otherwise whatever is in the "stack.yaml" file.
 STACKOPTS="--no-terminal --install-ghc --resolver=$RESOLVER"
@@ -32,18 +49,19 @@ if [[ -z "$SKIP_OLD_DEJAFU" ]]; then
   done
 fi
 
+# Test dpor-0.1 compat of dejafu
+if [[ -z "$SKIP_OLD_DPOR" ]]; then
+  # Use 0.1.0.1 because it builds with ghc 8.
+  sed 's:^- dpor$::' stack.yaml > stack-old-dpor.yaml
+  sed -i 's/^extra-deps: \[\]/extra-deps: [ dpor-0.1.0.1 ]/' stack-old-dpor.yaml
+
+  testdejafu "dejafu (dpor-0.1)" $STACKOPTS --stack-yaml=stack-old-dpor.yaml
+fi
+
 # Test HEAD version of everything
 testcmd "dpor" stack $STACKOPTS test dpor
 
-echo "== dejafu"
-if ! stack $STACKOPTS build dejafu-tests; then
-  echo "== FAILED"
-  exit 1
-fi
-if ! stack $STACKOPTS exec dejafu-tests; then
-  echo "== FAILED"
-  exit 1
-fi
+testdejafu "dejafu" $STACKOPTS
 
 for pkg in hunit-dejafu tasty-dejafu async-dejafu; do
   testcmd "${pkg}" stack $STACKOPTS test $pkg
